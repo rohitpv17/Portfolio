5b72915ecb0366a44f0a3ed85c9c3d8b26956153
rohitpv17 <rohitpv17@gmail.com>
2025-01-15 09:13:09 +0530

    Completed Contact Page

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+const sections = document.querySelectorAll("section");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector("header nav");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+});
+
+const activePage = () => {
+  navLinks.forEach((link) => {
+    const header = document.querySelector("header");
+    header.classList.remove("active");
+    setTimeout(() => {
+      header.classList.add("active");
+    }, 11);
+    link.classList.remove("active");
+  });
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+navLinks.forEach((link, idx) => {
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
+      activePage();
+      link.classList.add("active");
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 11);
+    }
+  });
+});
+
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
+    navLinks[0].classList.add("active");
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 11);
+  }
+});
+
 const resumeBtns = document.querySelectorAll(".resume-btn");
 resumeBtns.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
